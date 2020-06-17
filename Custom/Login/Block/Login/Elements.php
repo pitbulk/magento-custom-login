872dca1298220b9e40ac2daa60e48bc579dfe02c
Sixto Martin <pitbulk@gmail.com>
2020-06-17 11:31:17 +0200

    The Login controller now is processed as a POST

--- Custom/Login/Block/Login/Elements.php
+++ Custom/Login/Block/Login/Elements.php
@@ -9,19 +9,23 @@ namespace Custom\Login\Block\Login;
 
 use Magento\Framework\View\Element\AbstractBlock;
 use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\Data\Form\FormKey;
 use Magento\Store\Model\StoreManagerInterface;
 
 class Elements extends AbstractBlock
 {
     private $storeManager;
+    private $formkey;
 
     public function __construct(
         Context $context,
         StoreManagerInterface $storeManager,
-        array $data = []
+        array $data = [],
+        FormKey $formkey
     ) {
 
         $this->storeManager = $storeManager;
+        $this->formkey = $formkey;
         parent::__construct($context, $data);
     }
 
@@ -29,6 +33,7 @@ class Elements extends AbstractBlock
     {
         $html = '';
 
+        $formkey = $this->formkey->getFormKey();
         $loginUrl = $this->storeManager
                     ->getStore()
                     ->getUrl("mysso/custom/login");
@@ -38,8 +43,10 @@ class Elements extends AbstractBlock
        <strong role="heading">Custom Login</strong>
     </div>
     <div class="block-content">
-       <a class="action login primary"
-          href="'.$loginUrl.'">Custom login</a>
+       <form id="custom_login" action="'.$loginUrl.'" method="post" novalidate="novalidate">
+       <input name="form_key" type="hidden" value="'.$formkey.'">
+       <button type="submit" class="action login primary" name="Custom Login" id="customsend"><span>Custom Login</span></button>
+       </form>
     </div>';
         return $html;
     }
